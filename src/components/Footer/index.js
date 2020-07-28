aab128c4e933cc95f1131cabca2ad68fdca8a783
augustocalandrino <augustocalandrino@gmail.com>
2020-07-28 12:23:02 -0300

    Initial Project Setup

---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FooterBase, Autor } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <Autor>
+        &copy; 2020 | Augusto Calandrino
+      </Autor>
+      <hr style={{margin: "0 5%"}}/>
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura
+        </a>
+      </p>
+      <a href="https://www.alura.com.br/">
+        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      </a>
+    </FooterBase>
+  );
+}
+
+export default Footer;
