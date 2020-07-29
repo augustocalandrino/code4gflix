60f793b4889932935774a9d4a8066c71c6ff4ec2
augustocalandrino <augustocalandrino@gmail.com>
2020-07-28 23:37:22 -0300

    Updated dia#2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import Button from '../Button';
-//import ButtonLink from '../ButtonLink';
 import './Menu.css';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src={Logo} alt="Logotipo HyperFlix"/>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            <Link to="/">
+                <img className="Logo" src={Logo} alt="Logo code4gflix"/>
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Vídeo
             </Button>
         </nav>
