843b6777e7f72de93dba9ac942c0e7a7632e968d
augustocalandrino <augustocalandrino@gmail.com>
2020-07-31 22:40:38 -0300

    Video page add

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,46 +1,81 @@
-import React, { useEffect } from 'react';
-import Menu from '../../components/Menu';
-import dadosIniciais from '../../data/dados_iniciais.json';
+import React, { useEffect, useState } from 'react';
+// import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
+import PageDefault from '../../components/PageDefault';
 import categoriasRepository from '../../repositories/categorias';
 
 function Home() {
+  const [dadosIniciais, setDadosIniciais] = useState([]);
+
   useEffect(() => {
+    // http://localhost:8080/categorias?_embed=videos
     categoriasRepository.getAllWithVideos()
       .then((categoriasComVideos) => {
         console.log(categoriasComVideos);
+        setDadosIniciais(categoriasComVideos);
       })
       .catch((err) => {
         console.log(err.message);
       });
-  });
-  // http://localhost:8080/categorias?_embed=videos
+  }, []);
 
   return (
-    <div className="back">
-      <Menu />
-      <BannerMain
+    <PageDefault paddingAll={0}>
+      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
+
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription="Cursos | Notícias | Boas Práticas | Análises."
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
+            </div>
+          );
+        }
+
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
+
+      {/* <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription="Cursos | Notícias | Boas Práticas | Análises."
       />
+
       <Carousel
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
       />
+
       <Carousel
         category={dadosIniciais.categorias[1]}
       />
+
       <Carousel
         category={dadosIniciais.categorias[2]}
       />
+
       <Carousel
         category={dadosIniciais.categorias[3]}
       />
-      <Footer />
-    </div>
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      /> */}
+    </PageDefault>
   );
 }
 
